279b4d599f45d1e95981446712bf6620f484a8b9
Joel-Kaleb <joelkalebmoreno@gmail.com>
2025-09-25 20:02:04 -0600

    Create the static method to display contacts and adjust the menu

--- Program.cs
+++ Program.cs
@@ -6,15 +6,35 @@
         {
             int inputUser;
             string input;
+            ContactList agenda = new ContactList();
             do
             {
                 ShowMenu();
                 input = Console.ReadLine();
-                switch (input)
+
+                if (int.TryParse(input, out inputUser))
                 {
+                    switch (inputUser)
+                    {
+                        case 1:
+                            ViewContacts(agenda);
+                            break;
+                        case 5:
+                            Console.WriteLine("Saliendo del programa...");
+                            break;
+                        default:
+                            Console.WriteLine("La opción no es valida, precione enter para continuar");
+                            Console.ReadLine();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, ingrese un número. Presione Enter.");
+                    Console.ReadLine();
                 }
 
-            }while (!int.TryParse(input, out inputUser) || inputUser != 5);
+            }while (inputUser != 5);
         }
 
         static void ShowMenu()
@@ -27,6 +47,43 @@
             Console.WriteLine("4. Buscar Contacto Por Nombre");
             Console.WriteLine("5. Salir");
             Console.Write("Seleccione una opción: ");
+        }
+
+        static void ViewContacts(ContactList agenda)
+        {
+            Console.Clear();
+            Console.WriteLine("1. Mostrar orden ascendente");
+            Console.WriteLine("2. Mostrar orden descendente");
+            Console.WriteLine("3. Volver a menú principal");
+            Console.Write("Seleccione una opción: ");
+            string input = Console.ReadLine();
+            int inputNumber;
+
+            if (int.TryParse(input, out inputNumber) && (inputNumber >= 1 && inputNumber <= 3))
+            {
+                if (inputNumber == 1)
+                {
+                    agenda.DisplayContacts();
+                }
+                else if(inputNumber == 2) 
+                {
+                    agenda.PrintInReverseOrder();
+                }
+                else if(inputNumber == 3)
+                {
+                    return;
+                }
+
+                Console.WriteLine("\nPresione Enter para continuar...");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("La opción ingresada no es valida, volviendo al menú principal," +
+                    "precione enter para continuar");
+                Console.ReadLine();
+                return;
+            }
         }
     }
 }
